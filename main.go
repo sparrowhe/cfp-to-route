bc7a78585eadecf5ccedbfe99e44fc102b5971d3
SparrowHe <sparrowhe@gmail.com>
2022-07-08 16:32:15 +0800

    🔧 测试sqlite内存操作

--- main.go
+++ main.go
@@ -17,13 +17,31 @@ func dbInit(dbPath string) error {
 	return nil
 }
 
+// func CopyDatabaseToMem(dbPath string) error {
+// 	// 将数据库复制到内存中
+// 	DB, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+// 	if err != nil {
+// 		return err
+// 	}
+// 	DBMem, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+// 	DBMem.AutoMigrate(&model.Airway{}, &model.Airport{})
+// 	// 从磁盘写入数据
+// 	var waypoints []model.Airway
+// 	DB.Table("airways").Find(&waypoints)
+// 	// 分批写入内存
+// 	for i := 0; i < len(waypoints); i += 900 {
+// 		DBMem.Create(waypoints[i : i+900])
+// 	}
+// 	global.DB = DBMem
+// 	return nil
+// }
+
 func main() {
 	dbInit(global.DbPath)
 	global.LogInit()
 	httpServer := global.CreateWebServer()
 	controller.RouterInit(httpServer)
 	httpServer.Start(":8080")
-	// res, _ := service.SegmentToPointsList(service.ParseCFPRoute("APESO W615 IDAXI R473 BEMAG V5 CON A599 POU"))
 	// fmt.Println(res)
 	// for _, v := range res {
 	// 	fmt.Println(v.Name)
